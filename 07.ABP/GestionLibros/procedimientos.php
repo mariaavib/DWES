a34ccc62059553a7007eaa3f8d3d072d37192626
Maria <mvb.mvidigal@gmail.com>
2024-11-20 19:00:42 +0100

    Ejercicio POOJ ABP

--- 07.ABP/GestionLibros/procedimientos.php
+++ 07.ABP/GestionLibros/procedimientos.php
@@ -32,14 +32,15 @@ class Procedimientos {
 
     public function librosPorCurso() {
         $consulta = "SELECT libros.ISBN,libros.nombre AS titulo,clases.idCurso,clases.letraClase, reservas_libros.entregado
-FROM libros
-INNER JOIN reservas_libros 
-	ON  libros.ISBN = reservas_libros.ISBN
-INNER JOIN reservas 
-	ON reservas_libros.idReserva = reservas.idReserva
-INNER JOIN clases 
-	ON reservas.idCurso = clases.idCurso AND clases.idCurso = '".$_POST['clases']."'
-		AND reservas.letraClase = clases.letraClase;";
+                        FROM libros
+                        INNER JOIN reservas_libros 
+                            ON  libros.ISBN = reservas_libros.ISBN
+                        INNER JOIN reservas 
+                            ON reservas_libros.idReserva = reservas.idReserva
+                        INNER JOIN clases 
+                            ON reservas.idCurso = clases.idCurso AND clases.idCurso = '".$_POST['clases']."'
+		                        AND reservas.letraClase = clases.letraClase;";
+
         $resultado=$this->mysqli->query($consulta);
         for ($i = 0; $fila = $resultado->fetch_array(); $i++) {
             $this->isbn[$i] = $fila["ISBN"];
@@ -47,8 +48,21 @@ INNER JOIN clases
             $this->clases[$i] = $fila["idCurso"].' '.$fila["letraClase"];
             $this->activo[$i] = $fila["entregado"];
         }
-        /*print_r($isbn);*/  
     }
+
+    public function borrar(){
+        $consulta = 'DELETE FROM libros WHERE isbn='.$_POST["isbn"].';';
+        $resultado = $mysqli->query($consulta);
+
+        if($resultado){
+            echo "Libro borrado correctamente";
+        }else{
+            echo "Error";
+        }
+
+        header("Location:gestionLibros.php");
+    }
+    
     public function close() {
         $this->mysqli->close();
     }
