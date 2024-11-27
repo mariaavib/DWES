e0698c1693ed5401acde172b9867b41c3671091b
maria <mariavidigalbarroso.guadalupe@alumnado.fundacionloyola.net>
2024-11-27 13:54:32 +0100

    Ejercicio Equiquiz V2

---
+++ 08.Equiquiz/crearEscenario.sql
@@ -0,0 +1,19 @@
+-- Crear base de datos
+CREATE DATABASE equiquiz;
+
+-- Usar la base de datos
+USE equiquiz;
+
+-- Crear tabla Escenarios
+CREATE TABLE Escenarios (
+    idEscenario TINYINT UNSIGNED AUTO_INCREMENT,
+    ambito VARCHAR(25) NOT NULL,
+    rutaMapa VARCHAR(250) NOT NULL,
+    CONSTRAINT pk_escenarios PRIMARY KEY (idEscenario)
+);
+
+-- Insert en Escenarios
+INSERT INTO Escenarios (ambito, rutaMapa) VALUES
+('Salud', './assets/escenarios/EscenarioSalud.jpeg'),
+('Educación', './assets/escenarios/EscenarioEducacion.jpeg'),
+('Laboral', './assets/escenarios/EscenarioEducacion.jpeg');
