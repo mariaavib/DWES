f1e009747df5b94366f81513e8cf66808e1b1fd2
Maria <mvb.mvidigal@gmail.com>
2025-01-09 01:27:08 +0100

    Pruebas

--- 09.SQL/consultas.sql
+++ 09.SQL/consultas.sql
@@ -12,6 +12,7 @@ INNER JOIN individual
 
 /*** No salen filas ya que las primeras filas que salen del primer inner join no coinciden con ninguna fila del segundo inner join ***/
 
+/*Sacar un listado de todos los clientes, con su nombre, y si es una empresa el nombre del representante*/
 SELECT customer.cust_id, officer.fname AS nombreRepresentante,individual.fname AS nombreIndiv
 FROM customer
 LEFT JOIN 	business 
@@ -29,3 +30,28 @@ RIGHT JOIN customer
 	ON business.cust_id = customer.cust_id
 LEFT JOIN individual
 	ON customer.cust_id = individual.cust_id;
+
+/* Saber cuantos empleados hay*/
+
+SELECT COUNT(emp_id) AS numEmpleados
+FROM employee;
+
+/* Saber el nombre de los empleados que son jefes y su id */
+
+SELECT DISTINCT employee.emp_id, employee.fname
+FROM employee
+INNER JOIN employee AS jefes
+	ON employee.emp_id = jefes.superior_emp_id;
+
+/* Nombre de los empleados y nombre de los jefes */
+SELECT employee.emp_id AS idTrabajador, employee.fname AS nombreTrabajador, jefes.emp_id AS idJefe, jefes.fname AS nombreJefe
+FROM employee
+LEFT JOIN employee AS jefes
+    ON employee.superior_emp_id = jefes.emp_id;
+
+/* Quitar los empleados que no tengan asignados jefes */
+SELECT employee.emp_id AS idTrabajador, employee.fname AS nombreTrabajador, jefes.emp_id AS idJefe, jefes.fname AS nombreJefe
+FROM employee
+INNER JOIN employee AS jefes
+    ON employee.superior_emp_id = jefes.emp_id;
+	/*Solo se cumple la condicion, en cambio con en el left mostraba todos los empleados+los que tenian jefe*/
