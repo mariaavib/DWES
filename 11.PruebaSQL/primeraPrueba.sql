089a0479150f1189e54d37e2ba29b65d231e6aed
maria <mariavidigalbarroso.guadalupe@alumnado.fundacionloyola.net>
2025-01-27 12:26:11 +0100

    SQL || Resolución consultas pruebas

---
+++ 11.PruebaSQL/primeraPrueba.sql
@@ -0,0 +1,5 @@
+/*Mostrar el nombre del alumno que envía y del alumno que recibe el reconocimiento, además del texto del reconocimiento (descripción).*/
+
+/*Añadir una orden para que un alumno solo pueda recibir un reconocimiento (esto se hace en local)*/
+/*Añadir una orden para que el alumno pueda volver a recibir varios reconocimientos (esto se hace en local).*/
+/*Mostrar los nombres de los alumnos que no han enviado reconocimientos.*/
