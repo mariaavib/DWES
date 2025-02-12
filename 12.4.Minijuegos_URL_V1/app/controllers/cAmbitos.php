2a2010cbaa5ea54461ceacc5912308ba53bbbd92
maria <mariavidigalbarroso.guadalupe@alumnado.fundacionloyola.net>
2025-02-12 14:14:41 +0100

    Validar

--- 12.4.Minijuegos_URL_V1/app/controllers/cAmbitos.php
+++ 12.4.Minijuegos_URL_V1/app/controllers/cAmbitos.php
@@ -8,18 +8,21 @@
             $this->objModelo = new MAmbitos();
         }
 
+        //Vista principal que envia al modelo
         public function inicio(){
             $this->vista = 'formulario';
             $datos =  $this-> objModelo -> cogerAmbitos();    
             return $datos;
         }
 
+        //Vista que envia al modelo y le pasa como parametro un array
         public function mostrar(){
             $this->vista = 'mostrarMinijuegos';
             $datos = $this -> objModelo -> cogerMinijuegos($_POST['ambitos']);
             return $datos;
         }
 
+        //Vista que muestra el minijuego seleccionado, no es necesario enviar a la vista
         public function minijuegoSeleccionado(){
             $this->vista = 'minijuegoSeleccionado';
             return true;
