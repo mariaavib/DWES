444c8da78bca26215a1a487d07ae65405d8c12e0
Maria <mvb.mvidigal@gmail.com>
2025-02-18 20:38:56 +0100

    Get_result y bind_result

--- 12.5.InicioSesion/12.5.Inicio_SesionV2(bind_result)/script.sql
+++ 12.5.InicioSesion/12.5.Inicio_SesionV2(bind_result)/script.sql
@@ -2,6 +2,5 @@ CREATE TABLE usuario (
     idUsuario INT AUTO_INCREMENT,
     correo VARCHAR(50) NOT NULL,
     passw VARCHAR(30) NOT NULL,
-    perfil VARCHAR(50) NOT NULL,
     CONSTRAINT pk_idUsuario PRIMARY KEY (idUsuario)
 );
