8011966d60b7c78f63c4dc6581c0728d32d88927
Maria <mvb.mvidigal@gmail.com>
2025-03-05 17:59:00 +0100

    subida de archivos

---
+++ subirArchivos/files.php
@@ -0,0 +1,12 @@
+<?php
+    $rutaCarpeta = 'archivos/';
+    $filename = $_FILES["archivo"]["name"]; //Nombre original del archivo
+    $rutaTemp = $_FILES["archivo"]["tmp_name"]; // Ruta temporal del archivo
+
+    if(move_uploaded_file($rutaTemp, $rutaCarpeta . $filename)){
+        echo "Archivo subido correctamente";
+    }else{
+        echo "No se ha podido subir";
+    }
+
+?>
