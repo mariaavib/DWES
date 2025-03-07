6ae046611b73ba7aa195715c5b473380b8d47228
Maria <mvb.mvidigal@gmail.com>
2025-03-07 15:23:15 +0100

    P

---
+++ 14.Examen2_procesos/app/views/altas.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Altas minijuegos</title>
+</head>
+<body>
+    <h2>Datos del minijuego</h2>
+    <form action="" method="post">
+        <label for="nombre">Nombre del juego: </label>
+        <input type="text" name="nombre" id="nombre">
+        <select name="ambito" id="ambito">
+            <option value=""></option>
+        </select>
+        <label for="clase">Recomendado para: </label>
+        <input type="checkbox" name="eso" id="eso">ESO
+        <input type="checkbox" name="ciclos" id="ciclos">ciclos
+        <input type="checkbox" name="bachillerato" id="bachillerato">bachillerato
+    </form>
+</body>
+</html>
